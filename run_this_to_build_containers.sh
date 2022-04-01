c3d393ee96d5198cdbe0f00bab261134d6f91fd1
Daniel Zoba <daniel.sobe@sorben.com>
2022-04-01 13:31:04 +0200

    more doc

--- run_this_to_build_containers.sh
+++ run_this_to_build_containers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+echo "Update the .debs in web/ with recent versions of"
+echo "jitsi-meet-web"
+echo "jitsi-meet-web-config"
+echo "packages from the build in the jitsi-meet repo!"
+
 JITSI_RELEASE=unstable make
 
