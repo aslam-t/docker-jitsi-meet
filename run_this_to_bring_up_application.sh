aa37aa6a13e56cb60089da4118a82c1da5cae057
Daniel Zoba <daniel.sobe@sorben.com>
2022-06-01 10:49:44 +0200

    adjustments to jibri container to allow startup

--- run_this_to_bring_up_application.sh
+++ run_this_to_bring_up_application.sh
@@ -17,3 +17,27 @@ docker-compose -f docker-compose-latest.yml up -d
 
 ## access on https://serbski-inkubator.de:8443
 
+# Add recording & livestreaming to normal setup
+
+## set up alsa loop device
+
+### configure 5 capture/playback interfaces
+echo "options snd-aloop enable=1,1,1,1,1 index=0,1,2,3,4" > /etc/modprobe.d/alsa-loopback.conf
+
+### setup autoload the module
+echo "snd-aloop" >> /etc/modules
+
+### load module explicitly (if not loaded after reboot)
+modprobe snd_aloop
+
+### check that the module is loaded
+lsmod | grep snd_aloop
+
+## adjust config
+
+### ENABLE_RECORDING=1
+### JIBRI_LOGS_DIR=/config/logs/
+
+## different startup procedure
+
+docker-compose -f docker-compose-latest.yml -f jibri-latest.yml up -d
