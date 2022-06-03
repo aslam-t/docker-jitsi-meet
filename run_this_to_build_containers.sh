1f3879b9833b5178f605a6b56f8e6d09e0a618cc
Daniel Zoba <daniel.sobe@sorben.com>
2022-06-03 13:43:26 +0200

    remember how to refresh containers

--- run_this_to_build_containers.sh
+++ run_this_to_build_containers.sh
@@ -7,3 +7,10 @@ echo "packages from the build in the jitsi-meet repo!"
 
 JITSI_RELEASE=unstable make
 
+# or to update containers from docker hub:
+
+JITSI_RELEASE=unstable FORCE_REBUILD=1 make
+
+# if you want to keep known-good versions, tag the "latest" containers
+# for i in prosody web jvb jicofo jibri ;  do docker image tag jitsi/$i:latest jitsi/$i:latesttestok ; done
+
